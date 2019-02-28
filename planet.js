f862f42b2d9f3c88d6c8cf8a64915a06e507d638
NoNelso <jasonr4nd@gmail.com>
2019-02-27 22:26:47 -0500

    - single-color shading, added texture shapes and asignment.

--- planet.js
+++ planet.js
@@ -1,14 +1,18 @@
-function Planet(radius, distance, speed, angle, level, hue, wobble, vect) {
-  //create object with vars and dependant array
+function Planet(radius, distance, speed, angle, level, wobble,
+  vect, txtr) {
   this.r = radius;
   this.d = distance;
   this.s = speed;
   this.a = angle;
   this.l = level;
-  this.c = hue;
   this.w = wobble;
   this.v = vect;
   this.planets = [];
+  this.t = txtr;
+  noStroke();
+  nofill();
+  this.globe = createShape(SPHERE, this.r);
+  this.globe.setTexture(this.t);
 
   this.spin = function() {
     this.a += this.s; // base spin rate on speed
@@ -23,9 +27,10 @@ function Planet(radius, distance, speed, angle, level, hue, wobble, vect) {
       let dis = floor(random(100, 150) / lv); //each planet rand dist from parent
       let spd = random(-.075, .075); //each planet at rand orbit speed
       let ang = random(TWO_PI);
-      let col = color(floor(random(250)), floor(random(250)), floor(random(250)), 175);
       let lev = lv;
       let vec = p5.Vector.random3D();
+      if (lev == 2) let txt = plntxt[floor(random(4.999))];
+      else if (lev == 3) let txt = montxt[floor(random(1.999))];
       this.planets.push(new Planet(rad, dis, spd, ang, lev, col, 1, vec));
       if (lv < 3) { //if level not at cap spawn again
         this.planets[j].spawnSpiners(5 - lv, 1 + lv);
@@ -36,11 +41,9 @@ function Planet(radius, distance, speed, angle, level, hue, wobble, vect) {
 
   this.show = function() { //save state, move center, show, and reload state
     push();
-    // noStroke();
-    fill(this.c);
     rotate(this.a, this.v);
     translate(this.d, 0, 0);
-    sphere(this.r);
+    shape(globe);
     for (let k in this.planets) this.planets[k].show();
     pop();
   }
