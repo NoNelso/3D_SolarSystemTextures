e008ba0b2bb49bbfcb0883d8fcdcec92191912de
NoNelso <jasonr4nd@gmail.com>
2019-02-27 22:28:13 -0500

    edited comments

--- planet.js
+++ planet.js
@@ -1,5 +1,6 @@
 function Planet(radius, distance, speed, angle, level, wobble,
   vect, txtr) {
+  //create object with vars and array of orbiting dependants
   this.r = radius;
   this.d = distance;
   this.s = speed;
@@ -15,31 +16,46 @@ function Planet(radius, distance, speed, angle, level, wobble,
   this.globe.setTexture(this.t);
 
   this.spin = function() {
-    this.a += this.s; // base spin rate on speed
-    if (this.w == 1) this.d = this.d + sin(this.a); // if wobble set, wobbles as it rotates
-    for (let i in this.planets) this.planets[i].spin(); //recursive check dependants & re-call function
+    // base spin rate on speed
+    this.a += this.s;
+    // if wobble set, wobbles as it rotates
+    if (this.w == 1) this.d = this.d + sin(this.a);
+    //recursive check dependants & re-call function
+    for (let i in this.planets) this.planets[i].spin();
   }
 
   this.spawnSpiners = function(childNum, lv) {
-    console.log("C#", childNum, "LV", lv);
-    for (let j = 0; j < childNum; ++j) { //create planets by children number per lv
-      let rad = ceil(this.r / (lv + .3)); //each level is slightly smaler than parent
-      let dis = floor(random(100, 150) / lv); //each planet rand dist from parent
-      let spd = random(-.075, .075); //each planet at rand orbit speed
+    console.log("Cld #", childNum, "LV", lv);
+    //create planets by children number per lv
+    for (let j = 0; j < childNum; ++j) {
+      //each level is slightly smaler than parent
+      let rad = ceil(this.r / (lv + .3));
+      //each planet rand dist from parent
+      let dis = floor(random(100, 150) / lv);
+      //each planet at rand orbit speed
+      let spd = random(-.075, .075);
+      //start at random angle about axis
       let ang = random(TWO_PI);
+      //record planet level for debug sake
       let lev = lv;
+      //give random vector to orbit about
       let vec = p5.Vector.random3D();
+      //assign random texture from pool assigned based on level
       if (lev == 2) let txt = plntxt[floor(random(4.999))];
       else if (lev == 3) let txt = montxt[floor(random(1.999))];
+      //add new orbiting dependant to dependants array
       this.planets.push(new Planet(rad, dis, spd, ang, lev, col, 1, vec));
-      if (lv < 3) { //if level not at cap spawn again
-        this.planets[j].spawnSpiners(5 - lv, 1 + lv);
+      //if level of new dependant is not at the level cap, spawn again
+      if (lv < 3) {
+        this.planets[j].spawnSpiners(-1 + childNum, 1 + lv);
       }
     }
+    //after final spawn pause the draw loop
     noLoop();
   }
 
-  this.show = function() { //save state, move center, show, and reload state
+  //save state, move center, showobjects, show dependants, reload state
+  this.show = function() {
     push();
     rotate(this.a, this.v);
     translate(this.d, 0, 0);
